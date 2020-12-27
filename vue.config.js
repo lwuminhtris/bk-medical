8b435e4b4409b1bb33081dc988384ddb32804cfe
notm16 <minhtri.luu10@gmail.com>
2020-12-27 12:47:57 +0700

    minor bug fixed

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,4 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  dev: {
-    proxyTable: {
-      // proxy all requests starting with /api to jsonplaceholder
-      '/api': {
-        target: 'http://jsonplaceholder.typicode.com',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
-    }
-  }
 }
